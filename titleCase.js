a3cb8f31d2fce0014e33e7e86c304f7eee7bf953
brytek <dbryant@brytek.us>
2016-10-03 13:20:34 -0400

    Create titleCase.js

---
+++ titleCase.js
@@ -0,0 +1,11 @@
+// Return the provided string with the first letter of each word capitalized. 
+// Make sure the rest of the word is in lower case.
+
+function titleCase(str) {
+  str = str.toLowerCase();  
+  str = str.split(" ");  
+  for (var i = 0; i < str.length; i++) {
+    str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1); 
+  }  
+  return str.join(" ");  
+}
